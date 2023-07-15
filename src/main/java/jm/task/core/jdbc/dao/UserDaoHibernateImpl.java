87a4ae3198da92d45ac42cd045c376546a23aa7e
Crakozyabra <mrcrakozyabra@gmail.com>
2023-07-15 18:34:14 +0300

    complete

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,131 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
+import javax.persistence.PersistenceException;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+
+    private SessionFactory sessionFactory = Util.getSessionFactory();
+
     public UserDaoHibernateImpl() {
 
     }
 
-
     @Override
     public void createUsersTable() {
-
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            try {
+                session.createNativeQuery("""
+                            CREATE TABLE IF NOT EXISTS users(
+                                id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                                name VARCHAR(128),
+                                last_name VARCHAR(128),
+                                age TINYINT UNSIGNED
+                            );
+                        """).executeUpdate();
+                transaction.commit();
+            } catch (PersistenceException e) {
+                transaction.rollback();
+                e.printStackTrace();
+            }
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            try {
+                session.createNativeQuery("DROP TABLE IF EXISTS users").executeUpdate();
+                transaction.commit();
+            } catch (PersistenceException e) {
+                transaction.rollback();
+                e.printStackTrace();
+            }
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            try {
+                // https://docs.jboss.org/hibernate/orm/5.6/userguide/html_single/Hibernate_User_Guide.html#sql-query-parameters
+                session.createNativeQuery("INSERT INTO users(name, last_name, age) VALUES (:name, :lastName, :age);")
+                        .setParameter("name", name)
+                        .setParameter("lastName", lastName)
+                        .setParameter("age", age)
+                        .executeUpdate();
+                transaction.commit();
+            } catch (PersistenceException e) {
+                transaction.rollback();
+                e.printStackTrace();
+            }
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            try {
+                session.createNativeQuery("DELETE FROM users WHERE id=:id").setParameter("id", id).executeUpdate();
+                transaction.commit();
+            } catch (PersistenceException e) {
+                transaction.rollback();
+                e.printStackTrace();
+            }
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            try {
+                // https://docs.jboss.org/hibernate/orm/5.6/userguide/html_single/Hibernate_User_Guide.html#sql-entity-query
+                List<User> users = session.createNativeQuery("SELECT * FROM users", User.class).list();
+                transaction.commit();
+                return users;
+            } catch (PersistenceException e) {
+                transaction.rollback();
+                e.printStackTrace();
+            }
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            try {
+                session.createNativeQuery("DELETE FROM users").executeUpdate();
+                transaction.commit();
+            } catch (PersistenceException e) {
+                transaction.rollback();
+                e.printStackTrace();
+            }
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,13 +1,53 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/jdbc?createDatabaseIfNotExist=true";
+
+    private static final String USER = "root";
+
+    private static final String PASSWORD = "root";
+
+    private static SessionFactory sessionFactory;
+
+    private static void initSessionFactory() {
+        Properties properties = new Properties();
+        properties.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+        properties.put(Environment.URL, URL);
+        properties.put(Environment.USER, USER);
+        properties.put(Environment.PASS, PASSWORD);
+        properties.put(Environment.SHOW_SQL, true);
+        properties.put(Environment.FORMAT_SQL, true);
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+
+        sessionFactory = new Configuration().
+                addProperties(properties).
+                addAnnotatedClass(User.class).
+                buildSessionFactory();
+    }
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            initSessionFactory();
+        }
+        return sessionFactory;
+    }
+
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc?createDatabaseIfNotExist=true",
-                "root", "root");
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public Util() {
+        throw new UnsupportedOperationException();
     }
 }
